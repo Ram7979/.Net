7e80c6c31cb0d58f62ebbb7a9cf5330737c51369
Ram Naidu Sisti <118529741+Ram7979@users.noreply.github.com>
2025-12-22 23:26:48 +0530

    Add files via upload

---
+++ Day4OOPDemo/Dat4OOPDemoconsole/ClassDemo.cs
@@ -0,0 +1,16 @@
+using namespace;
+namespace Data4OOPDemo{
+    class Program{
+        class Employee{
+            #region Properties
+            public int employeeID {get; set;}
+            public string employeeName {get; set;}
+        }
+
+
+        stataic void Main(String[] args){
+            ClassDemo demoObj = new ClassDemo("Ram");
+            clasDemo d1 = new ClassDemo();
+        }
+    }
+}
