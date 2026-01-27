b2cb79e2e4bd6aa0e745b03b458115fe04c339a5
Ram Naidu Sisti <118529741+Ram7979@users.noreply.github.com>
2026-01-27 11:41:37 +0530

    Add files via upload

---
+++ Top Brains Assessments/Multiplicationtable/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class program{
+    public static void Main(string[] args){
+        int n = int.Parse(Console.ReadLine());
+        int upto = int.Parse(Console.ReadLine());
+        for(int i=1;i<=upto;i++){
+            int ans = n * i;
+            Console.WriteLine(ans);
+
+        }
+
+
+    }
+}
