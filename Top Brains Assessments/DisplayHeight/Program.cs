0ea654723a2ee0e643bb0f29d3ea9742f51b292d
Ram Naidu Sisti <118529741+Ram7979@users.noreply.github.com>
2026-01-27 12:55:33 +0530

    Add files via upload

---
+++ Top Brains Assessments/DisplayHeight/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program{
+    public string PersonHeight(int h){
+        if(h < 150){
+            return "Short";
+
+        }
+        else if(h <= 150 && h<180){
+            return "Average";
+
+        }
+        return "Tall";
+
+    }
+    public static void Main(string[] args){
+        int height = int.Parse(Console.ReadLine());
+        Program p = new Program();
+        string res = p.PersonHeight(height);
+        Console.WriteLine(res);
+
+    }
+}
