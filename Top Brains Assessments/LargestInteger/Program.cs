cfd07dbfb38e23caf16fa89fd285fb693d7e294e
Ram Naidu Sisti <118529741+Ram7979@users.noreply.github.com>
2026-01-27 12:11:49 +0530

    Add files via upload

---
+++ Top Brains Assessments/LargestInteger/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program{
+    public int Largest(int a, int b, int c){
+        if(a>=b && a>=c){
+            return a;
+
+        }
+        else if(b>=a && b>=c){
+            return b;
+
+        }
+        return c;
+
+
+
+    }
+    public static void Main(string[] args){
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        Program p=  new Program();
+        int ans = p.Largest(a,b,c);
+        Console.WriteLine(ans);
+
+        
+
+    }
+}
